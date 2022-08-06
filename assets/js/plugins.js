b3bbb5d47cfaa580c07bffe859a9af7ab04002b2
Benjamin James <benjames@mit.edu>
2022-08-06 18:18:24 -0400

    migrations added

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,1 +1,10 @@
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
+function entryradio() {
+    var row, el;
+    $("input[type=radio]").click(function() {
+	el = $(this);
+	row = el.data("row");
+	$("input[data-row=" + row + "]").prop("checked", false);
+	el.prop("checked", true);
+    });
+}
